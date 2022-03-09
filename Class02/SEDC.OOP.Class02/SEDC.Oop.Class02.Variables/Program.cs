fd4bc8802964ff28adc6c1916707e36e717cd918
anetastankovska <anetastankovskaane@gmail.com>
2022-03-09 22:39:15 +0100

    Added class 2

---
+++ Class02/SEDC.OOP.Class02/SEDC.Oop.Class02.Variables/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace SEDC.Oop.Class02.Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SEDC!");
+
+            // let a = 3; (in JavaScript)
+            // Brojki
+            int number = 3;
+            number = 123456; // Cannot put big number in int
+            long bigNumber = 12345678909874456;
+            short shortNumber = 12000;
+            byte smallNumber = 8;
+
+            double doublePrecisionNumber = 12.093123;
+            float singlePrecisionNumber = 123.1f;
+
+            string str = "Trajan Stevkovski"; // double quotes
+            char character = 'a'; // single quotes
+
+            bool isSomethingTrue = false;
+
+            DateTime currentDate = new DateTime();
+
+            int module = 10;
+            module %= 3;
+            Console.WriteLine(module);
+
+
+            var a = 34567;
+
+
+            //Console.WriteLine("================== DATA ENTRY ================");
+
+            //string input = Console.ReadLine();
+
+            //Console.WriteLine("This is the input value:" + input);
+
+
+            //int parsedToInteger = int.Parse(input);
+            //Console.WriteLine("This is the parsed data: " + input);
+
+            //bool paresedToBoolean = Convert.ToBoolean(input);
+            //Console.WriteLine("This is the parsed input value: " + paresedToBoolean);
+
+            //long parsedToLong = 0;
+            //bool isValidIput = long.TryParse(input, out parsedToLong);
+
+            //bool isValidIput1 = long.TryParse(input, out long parsedToLong1);
+            //Console.WriteLine("Is parsing1 valid: " + isValidIput1);
+            //Console.WriteLine("This is the parsed value: " + parsedToLong1);
+
+            //Console.WriteLine("Is parsing valid: " + isValidIput);
+            //Console.WriteLine("This is the parsed value: " + parsedToLong);
+
+
+            //string parsedToString = parsedToLong.ToString();
+
+
+            Console.WriteLine("================= BRANCHING ==================");
+
+            string input = Console.ReadLine();
+
+            bool isValidInteger = int.TryParse(input, out int parsedInput);
+
+            if (isValidInteger)
+            {
+                Console.WriteLine("The parsed number is: " + parsedInput);
+            } else
+            {
+                Console.WriteLine("Not valid input!");
+            }
+
+            int firstNumber = 5;
+            int secondNumber = 10;
+            int thirdNumber = 15;
+            int fourthNumber = 20;
+
+            if (firstNumber < secondNumber && firstNumber > thirdNumber)
+            {
+                Console.WriteLine("The if statement is true");
+            } 
+            else if (thirdNumber < fourthNumber || secondNumber > fourthNumber)
+            {
+                Console.WriteLine("The first else if statement is TRUE");
+            } 
+            else if (firstNumber < secondNumber)
+            {
+                Console.WriteLine("The second else is statement is TRUE");
+            } 
+            else if (!(firstNumber < secondNumber))
+            {
+                Console.WriteLine("The third else is statement is TRUE");
+            }
+            else
+            {
+                Console.WriteLine("We are in else");
+            }
+
+
+            Console.WriteLine("====== SWITCH ======");
+
+            int someNumber = 3;
+
+            switch (someNumber)
+            {
+                case 1:
+                    break;
+                // case 'a':
+                // break;
+                // case "Something":
+                // break;
+                case 2:
+                    Console.WriteLine("We are at case 2");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("We are at case 3, 4, 5");
+                    break;
+                default:
+                    Console.WriteLine("If non of the cases is executed I'm always executed");
+                    break;
+            }
+
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
---
+++ Class02/SEDC.Oop.Class02.Exercises/SEDC.Oop.Class02.Exercises/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace SEDC.Oop.Class02.Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double doubleOne = 9.09569823;
+            double doubleTwo = 3.12458792;
+
+            double resultDouble = doubleOne / doubleTwo;
+            Console.WriteLine(resultDouble);
+
+            int intOne = 25;
+            int intTwo = 7;
+
+            int resultInt = intOne / intTwo;
+            Console.WriteLine(resultInt);
+
+
+            string strOne = "Aneta";
+            string strTwo = "Stankovska";
+
+            string resultStr = strOne + strTwo;
+            Console.WriteLine(resultStr);
+
+            string strNumOne = "9";
+            string strNumTwo = "3";
+
+            string resultStrTwo = strNumOne + strNumTwo;
+            Console.WriteLine(resultStrTwo);
+
+
+            int integerOne = 9;
+            string stringOne = "Ane";
+
+            string resultIntAndStr = integerOne + stringOne;
+            Console.WriteLine(resultIntAndStr);
+
+
+            int n = 102;
+            int m = 5;
+            int totalSms = n / m;
+            Console.WriteLine(totalSms);
+
+            Console.WriteLine("=======================");
+            int applesPerBranch = 8;
+            int applesPerBasket = 5;
+
+            float result = 12 * applesPerBranch / applesPerBasket;
+            Console.WriteLine(result);
+
+            Console.WriteLine("=======================================================");
+
+            string inputOne = Console.ReadLine();
+            string inputTwo = Console.ReadLine();
+
+            bool isValidIntegerOne = int.TryParse(inputOne, out int parsedInputOne);
+            bool isValidIntegerTwo = int.TryParse(inputTwo, out int parsedInputTwo);
+
+            if (isValidIntegerOne && isValidIntegerTwo)
+            {
+
+
+
+                Console.WriteLine(parsedInputOne);
+                Console.WriteLine(parsedInputTwo);
+
+
+                if (parsedInputOne > parsedInputTwo)
+                {
+                    Console.WriteLine("The first input is larger than second input");
+                }
+                else if (parsedInputOne < parsedInputTwo)
+                {
+                    Console.WriteLine("The second input is greater than the first");
+                }
+                else
+                {
+                    Console.WriteLine("They are equal");
+                }
+
+                if (parsedInputOne % 2 == 0)
+                {
+                    Console.WriteLine("The first input is even");
+                }
+                else if (parsedInputOne % 2 != 0)
+                {
+                    Console.WriteLine("The first input is odd");
+                }
+
+
+                if (parsedInputTwo % 2 == 0)
+                {
+                    Console.WriteLine("The second input is even");
+                }
+                else if (parsedInputTwo % 2 != 0)
+                {
+                    Console.WriteLine("The second input is odd");
+                }
+
+                string inputNumFirst = Console.ReadLine();
+
+                bool isValidInput = int.TryParse(inputNumFirst, out int parsedInput);
+
+                switch (parsedInput)
+                {
+                    case 1:
+                        Console.WriteLine("You got a new car!");
+                        break;
+                    case 2:
+                        Console.WriteLine("You got a");
+                        break;
+                    case 3:
+                        Console.WriteLine("You got a");
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        break;
+                }
+
+
+
+            }
+
+            
+            
+        }
+    }
+}
