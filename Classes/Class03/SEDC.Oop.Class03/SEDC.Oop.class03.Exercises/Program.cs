8b37ea265438aac93ad9b04d4c101b8607cf08fe
anetastankovska <anetastankovskaane@gmail.com>
2022-03-13 18:53:11 +0100

    Refactor exercises for loops

--- Classes/Class03/SEDC.Oop.Class03/SEDC.Oop.class03.Exercises/Program.cs
+++ Classes/Class03/SEDC.Oop.Class03/SEDC.Oop.class03.Exercises/Program.cs
@@ -4,79 +4,121 @@ namespace SEDC.Oop.class02.Exercises
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void PrintNumbersAscAndDecs()
+        {
+            //Get an input number from the console. Print all numbers from 1 to that number
+            //Get another input number from the console. Print all numbers from that number to 1
+
+            Console.WriteLine("Eneter the first number:");
+            string inputone = Console.ReadLine();
+
+            bool isvalidinputOne = int.TryParse(inputone, out int parsedinputone);
+
+            if (!isvalidinputOne)
+            {
+                Console.WriteLine("Enter a valid integer!");
+                return;
+            }
+
+            for (int i = 0; i <= parsedinputone; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.WriteLine("Enter the second number:");
+            string inputTwo = Console.ReadLine();
+
+            bool isValidInputTwo = int.TryParse(inputTwo, out int parsedInputTwo);
+
+            if (!isValidInputTwo)
+            {
+                Console.WriteLine("Enter a valid integer!");
+            }
+            for (int i = parsedInputTwo; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void PrintOddAndEvenNumbers()
+        {
+            //Get an input number from the console. Print all even numbers starting from 2
+            //Get another input number  from the console. Print all odd numbers  starting from 1
+
+            Console.WriteLine("Eneter a first number:");
+            string inputOne = Console.ReadLine();
+
+            bool isValidInputOne = int.TryParse(inputOne, out int parsedInputOne);
+            if (!isValidInputOne)
+            {
+                Console.WriteLine("Enter a valid integer!");
+                return;
+            }
+
+            for (int i = 2; i <= parsedInputOne; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Enter a second number");
+            string inputTwo = Console.ReadLine();
+
+            bool isValidInputTwo = int.TryParse(inputTwo, out int parsedInputTwo);
+
+            if (!isValidInputTwo)
+            {
+                Console.WriteLine("Enter a valid integer!");
+                return;
+            }
+
+            for (int i = 0; i <= parsedInputTwo; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+
+        public static void PruntToHundred()
         {
 
-            //Console.WriteLine("Eneter the first number:");
-            //string inputOne = Console.ReadLine();
+            //Get an input from the console. Print all numbers from 1 to that number. Don’t print numbers that can be divided by 3 or 7
+            //If the number gets to 100 print a message: The limit is reached and stop counting
 
-            //bool isValidInputOne = int.TryParse(inputOne, out int parsedInputOne);
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
 
-            //for(int i = 0; i <= parsedInputOne; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            bool isValidInput = int.TryParse(input, out int parsedInput);
 
+            for (int i = 0; i < parsedInput; i++)
+            {
+                if (i > 100)
+                {
+                    Console.WriteLine("The limit is reached. You cannot entern numbers greater than 100");
+                    break;
+                }
+                if (i % 3 != 0 && i % 7 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
 
-            //Console.WriteLine("Enter the second number:");
-            //string inputTwo = Console.ReadLine();
+        private static void FizzBuzz()
+        {
 
-            //bool isValidInputTwo = int.TryParse(inputTwo, out int parsedInputTwo);
-            //for(int i = parsedInputTwo; i > 0; i--)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            //Write a program that prints the numbers from 1 to 100.
+            //But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+            //For numbers which are multiples of both three and five print “FizzBuzz
 
-
-            //Console.WriteLine("Eneter a first number:");
-            //string inputOne = Console.ReadLine();
-
-            //bool isValidInputOne = int.TryParse(inputOne, out int parsedInputOne);
-
-            //for(int i = 2; i <= parsedInputOne; i++)
-            //{
-            //    if (i % 2 == 0)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //}
-
-            //Console.WriteLine("Enter a second number");
-            //string inputTwo = Console.ReadLine();
-
-            //bool isValidInputTwo = int.TryParse(inputTwo, out int parsedInputTwo);
-
-            //for(int i = 0; i <= parsedInputTwo; i++)
-            //{
-            //    if (i %2 != 0)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //}
-
-
-
-            //Console.WriteLine("Enter a number:");
-            //string input = Console.ReadLine();
-
-            //bool isValidInput = int.TryParse(input, out int parsedInput);
-
-            //for(int i = 0; i < parsedInput; i++)
-            //{
-            //    if(i > 100)
-            //    {
-            //        Console.WriteLine("You cannot entern numbers greater than 100");
-            //        break;
-            //    }
-            //    if (i % 3 != 0 && i % 7 != 0)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //}
-
-
-            Console.WriteLine("FIZZ/BUZZ task");
-            for(int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
                 {
@@ -93,10 +135,19 @@ namespace SEDC.Oop.class02.Exercises
                     Console.WriteLine("Buzz");
                     continue;
                 }
-                
                 Console.WriteLine(i);
-
             }
+        }
+
+        static void Main(string[] args)
+        {
+            //PrintNumbersAscAndDesc();
+
+            //PrintOddAndEvenNumbers();
+
+            //PruntToHundred();
+
+            //FizzBuzz();
         }
     }
 }
---
+++ Homeworks/Class03.homweork/Cllass03.homework/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+//Task 1
+//Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+
+//Test Data:
+//Enter integer no.1:
+//4
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//7
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//2
+//Enter integer no.1:
+//8
+//Expected Output:
+//The result is: 14
+
+
+//Task 2
+//Make a new console application called StudentGroup
+//Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+//Get a number from the console between 1 and 2 and print the students from that group in the console.
+//Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+//Test Data:
+//Enter student group: (there are 1 and 2 )
+//1
+//Expected Output:
+//The Students in G1 are:
+//Zdravko
+//Petko
+//Stanko
+//Branko
+//Trajko
+
+namespace Cllass03.homework
+{
+    class Program
+    {
+        // Task 1
+        public static void SumOfEven()
+        {
+            int[] numbers = new int[6];
+            int sumOfEvenNumbers = 0;
+
+            string response = "Y";
+
+            while (response == "Y")
+            {
+                Console.Clear();
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine($"Enter integer no. {i + 1}");
+                    string input = Console.ReadLine();
+                    bool isValidInput = int.TryParse(input, out int parsedInput);
+                    if (!isValidInput)
+                    {
+                        Console.WriteLine("The input is invalid!");
+                        return;
+                    }
+                    numbers[i] = parsedInput;
+
+                    if (parsedInput % 2 == 0)
+                    {
+                        sumOfEvenNumbers += parsedInput;
+                    }
+                }
+                string joinedArr = string.Join(',', numbers);
+                Console.WriteLine(joinedArr);
+                Console.WriteLine($"The sum of even numbers is: {sumOfEvenNumbers}");
+
+                Console.WriteLine("Do you want to continue? If yes, press Y, if no press N");
+
+                response = Console.ReadLine().ToUpper();
+
+                if (response == "N" && response != "Y")
+                {
+                    Console.WriteLine("Thank you for using our app!");
+                } 
+                else
+                {
+                    Console.WriteLine("Wrong command!");
+                }
+            }
+        }
+
+
+        public static void StudentGroup()
+        {
+
+            string[] group1 = { "Stefan", "Aneta", "Aleksandar", "Maja", "Kristijan" };
+            string[] group2 = { "Ilija", "Marija", "Slave", "Igor", "Ana" };
+            string response = "Y";
+
+            while (response == "Y")
+            {
+                Console.Clear();
+
+                Console.WriteLine("If you want to get students of group 1, enetr 1. If you want to get students from group 2, enter 2");
+                string selection = Console.ReadLine();
+
+                if (selection != "1" && selection != "2")
+                {
+                    Console.WriteLine("Your selection did not provide any results! Try again!");
+                }
+                else if (selection == "1")
+                {
+                    Console.WriteLine("The students in G1 are:");
+                    foreach (string student in group1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+                else if (selection == "2")
+                {
+                    Console.WriteLine("The students in G2 are:");
+                    foreach (string student in group2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+
+                Console.WriteLine("Do you want to check students for another group?");
+                response = Console.ReadLine().ToUpper();
+
+                if (response == "N" && response != "Y")
+                {
+                    Console.WriteLine("Thank you for using our app!");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong command!");
+                }
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            SumOfEven();
+            //StudentGroup();
+        }
+    }
+}
