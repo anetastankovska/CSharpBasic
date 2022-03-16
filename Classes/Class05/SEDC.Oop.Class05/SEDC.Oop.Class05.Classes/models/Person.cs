bc5f569ce7da237aaf641b17228fb0ec17c5d0b2
anetastankovska <anetastankovskaane@gmail.com>
2022-03-16 18:31:48 +0100

    Created Class 05

---
+++ Classes/Class05/SEDC.Oop.Class05/SEDC.Oop.Class05.Classes/models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Oop.Class05.Classes.models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        private long SSN { get; set; }
+
+        private long GenerateSSN()
+        {
+            Random rand = new Random();
+            long ssn = rand.Next(100_000, 999_999);
+            return ssn;
+        }
+
+        public void Talk(string text)
+        {
+            Console.WriteLine($"The human named {Name}(SSN: {SSN}) is saying {text}");
+        }
+    }
+}
