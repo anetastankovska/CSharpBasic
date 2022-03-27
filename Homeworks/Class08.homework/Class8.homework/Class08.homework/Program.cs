0f93745aa6fd96543cbbc60c7e8fe0ef7d8494f7
anetastankovska <anetastankovskaane@gmail.com>
2022-03-27 18:27:44 +0200

    Addec homework for class 8

---
+++ Homeworks/Class08.homework/Class8.homework/Class08.homework/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Class08.homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queueOfNumbers = new Queue<int>();
+            string response = "Y";
+            while(response == "Y")
+            {
+                ValidateInteger(queueOfNumbers);
+                Console.WriteLine("Do you want to add another number in the queue? Y/N");
+                response = Console.ReadLine();
+                if(response == "N")
+                {
+                    Console.WriteLine("Thank you for using our app. The numbers you entered in the queue are the following:");
+                    PrintNumbersFromQueue(queueOfNumbers);
+                }
+            }
+            Console.ReadLine();
+        }
+
+
+        public static int ValidateInteger (Queue<int> queue)
+        {
+            Console.WriteLine("Enter a number");
+            
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                bool isValidInt = int.TryParse(userInput, out int parsedInt);
+                if (!isValidInt)
+                {
+                    Console.WriteLine("The entered input cannot be converted to a valid integer. Try again!");
+                }
+                else {
+                    queue.Enqueue(parsedInt);
+                    return parsedInt;
+                };
+                
+            } 
+        }
+
+        public static void PrintNumbersFromQueue(Queue<int> queue)
+        {
+            foreach (int num in queue)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
