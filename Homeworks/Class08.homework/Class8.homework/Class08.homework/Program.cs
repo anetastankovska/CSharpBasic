f3d2d0d973ddd7680def93721583a74305768066
anetastankovska <anetastankovskaane@gmail.com>
2022-03-27 18:32:31 +0200

    Minor changes

--- Homeworks/Class08.homework/Class8.homework/Class08.homework/Program.cs
+++ Homeworks/Class08.homework/Class8.homework/Class08.homework/Program.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//Task 1
+//Give the user an option to input numbers
+//After inputing each number ask them if they want to input another with a Y/N question
+//Store all numbers in a QUEUE
+//When the user is done adding numbers print the number in the order that the user entered them from the QUEUE
+
 namespace Class08.homework
 {
     class Program
