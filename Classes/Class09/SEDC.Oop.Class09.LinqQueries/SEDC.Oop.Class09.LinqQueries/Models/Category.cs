ae40309bedad7865dfd08e8902020d6a7bd8b6be
anetastankovska <anetastankovskaane@gmail.com>
2022-03-28 19:01:41 +0200

    Created class 08

---
+++ Classes/Class09/SEDC.Oop.Class09.LinqQueries/SEDC.Oop.Class09.LinqQueries/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Oop.Class09.LinqQueries.Models
+{
+    public enum Category
+    {
+        ItEquipment,
+        LapTop,
+        PC,
+        TV
+    }
+}
